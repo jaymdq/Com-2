eea8ed7cdcc6deec640be0da97cf0b9e878545ed
helloviru <sebaviru@gmail.com>
2014-12-08 19:18:08 -0300

    Algunos comentarios

--- src/taskmanager/AP.java
+++ src/taskmanager/AP.java
@@ -1,5 +1,7 @@
 package taskmanager;
 
+import java.util.Date;
+
 public class AP {
 
 	public static final String TITLE = "BSSID		PWR		BEACONS		CH	ENC	ESSID";
@@ -9,9 +11,19 @@ public class AP {
 	public int channel;
 	public String encrypt;
 	public String essid;
-
+	public Date last;
+	
 	// String que iria en la consola
 	public String toString() {
-		return "";
+		// TODO Aca deberia imprimri los datos del AP de forma tabulada 
+		return bssid + power + beacons + channel + encrypt + essid;
+	}
+	
+	public boolean update(String packet) {
+		// TODO recibo un nuevo paquete de este ap
+		// Actualizo la fecha
+		// Si actualizo alguna otra cosa (pwr, beacons, ch, etc.. retorno true, sino false)
+		// Si retorno true, luego debo updatear la consola
+		return false;
 	}
 }
--- src/taskmanager/Client.java
+++ src/taskmanager/Client.java
@@ -1,5 +1,6 @@
 package taskmanager;
 
+import java.util.Date;
 import java.util.Vector;
 
 public class Client {
@@ -10,9 +11,19 @@ public class Client {
 	public String station;
 	public int packets;
 	public Vector<String> probes;
+	public Date last;
 	
 	// String que iria en la consola
 	public String toString() {
-		return "";
+		// TODO Aca deberia imprimri los datos del cliente de forma tabulada 
+		return bssid + station + power + packets + probes.toString();
+	}
+	
+	public boolean update(String packet) {
+		// TODO recibo un nuevo paquete de este dispositivo
+		// Actualizo la fecha
+		// Si actualizo alguna otra cosa (pwr, probes, etc.. retorno true, sino false)
+		// Si retorno true, luego debo updatear la consola
+		return false;
 	}
 }
